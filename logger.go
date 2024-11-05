43a91416d12b214833d74ea7ba77a88d7b74db06
rtzgod <azimaidynuly1@gmail.com>
2024-11-05 15:45:20 +0500

    err func

--- logger.go
+++ logger.go
@@ -30,3 +30,10 @@ func SetupLogger(env string) *slog.Logger {
 	}
 	return log
 }
+
+func Err(err error) slog.Attr {
+	return slog.Attr{
+		Key:   "error",
+		Value: slog.StringValue(err.Error()),
+	}
+}
